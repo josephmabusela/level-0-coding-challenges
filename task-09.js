693128fb1efb621f59cc6baaa415508c42b6531b
josephmabusela <josephmabusela5@gmail.com>
2021-10-11 11:39:05 +0200

    fixing functions

--- task-09.js
+++ task-09.js
@@ -1,14 +1,15 @@
-const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-
 function countVowels(sentence) {
-  let counts = 0;
-  for(let i = 0; i < vowels.length; i++) {
-    if(vowels.includes(sentence[i])) {
-      counts++;
-    }
+
+  var uniqueVowels = '';
+  var vowels = sentence.match(/[aeiou]/g).join(","); // Extracting the vowels
+
+  for(let i = 0; i < sentence.length; i++) {
+      if(uniqueVowels.indexOf(vowels.charAt(i)) < 0) {  
+        uniqueVowels += vowels.charAt(i);
+      }  
   }
-  return console.log(counts);
+  console.log('Vowels: ' + uniqueVowels);
 }
 
-countVowels('Hello World');
+countVowels("UmuziU");
 
